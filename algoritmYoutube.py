601fd44370f221b3be7dd311cc26e3fa1775444e
DarynaLisova29 <developelisova@gmail.com>
2024-08-01 18:51:15 +0300

    Create function and test

--- algoritmYoutube.py
+++ algoritmYoutube.py
@@ -1,27 +1,39 @@
 import cv2
 import pytesseract
 from imutils import contours
+from readNameFile import *
 
+def extract_text_from_image(image_path, min_area=5000):
 
-pytesseract.pytesseract.tesseract_cmd='C:/Users/Windows10/AppData/Local/Programs/Tesseract-OCR/tesseract.exe'
+    pytesseract.pytesseract.tesseract_cmd = 'C:\Program Files\Tesseract-OCR/tesseract.exe'
 
-image=cv2.imread("picture/ex3.jpg")
-height, width, _ = image.shape
-gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-thresh=cv2.threshold(gray, 0,255,cv2.THRESH_OTSU)[1]
-cnts=cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-cnts, _ =contours.sort_contours(cnts[0])
-# print(cnts)
+    image = cv2.imread(image_path)
+    height, width, _ = image.shape
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU)[1]
+    cnts = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cnts, _ = contours.sort_contours(cnts[0])
 
-for c in cnts:
-    area=cv2.contourArea(c)
-    x,y,w,h=cv2.boundingRect(c)
-    if area>5000:
-        img=image[y:y+h, x:x+w]
-        result=pytesseract.image_to_string(img, lang="rus+eng")
-        if len(result)>7:
-            print(result)
-cv2.imshow("Test",thresh)
+    results = []
+    for c in cnts:
+        area = cv2.contourArea(c)
+        x, y, w, h = cv2.boundingRect(c)
+        if area > min_area:
+            img = image[y:y + h, x:x + w]
+            result = pytesseract.image_to_string(img, lang="rus+eng")
+            if len(result) > 7:
+                results.append(result)
 
+    if len(results) == 1:
+        return results[0]
+    else:
+        return results
 
-cv2.waitKey()
+folder_path = 'picture1'
+file_list = get_file_list(folder_path)
+i=1;
+for file_name in file_list:
+    file=folder_path+"/"+file_name
+    # print(i)
+    print(i, extract_text_from_image(file))
+    i=i+1
